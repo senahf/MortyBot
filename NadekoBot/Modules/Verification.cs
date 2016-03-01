72d065088ee5525fa74f09aa00758caedffdbd8a
Matt <metchutran@gmail.com>
2016-03-01 15:45:35 -0800

    Added exe build; start Verify module

---
+++ NadekoBot/Modules/Verification.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord.Modules;
+
+namespace NadekoBot.Modules
+{
+    class Verification : DiscordModule
+    {
+        public Verification()
+        {
+            //new Thread(x => CheckVerify(e)).Start();
+        }
+        private async void CheckVerify()
+        {
+
+        }
+        public override void Install(ModuleManager manager)
+        {
+            
+        }
+    }
+}
