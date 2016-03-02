cbc0f7cb2afc561654ea3d98d562a6a8e8446108
Matt <metchutran@gmail.com>
2016-03-01 21:06:59 -0800

    Add base for reminder module

---
+++ NadekoBot/Modules/Reminders.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Modules;
+
+namespace NadekoBot.Modules
+{
+    class Reminders : DiscordModule
+    {
+        public Reminders()
+        {
+           
+        }
+        public override void Install(ModuleManager manager)
+        {
+            
+            manager.CreateCommands("!remind", cgb =>
+            {
+                cgb.AddCheck(Classes.Permissions.PermissionChecker.Instance);
+
+                commands.ForEach(com => com.Init(cgb));
+
+            });
+        }
+    }
+}
